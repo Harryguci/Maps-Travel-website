6518869f819f4c9da7fa42e231dce36cf0735afc
Harryguci <huyquang0028@gmail.com>
2023-08-01 11:39:52 +0700

    Added create reviews, send Images

--- README.md
+++ README.md
@@ -6,3 +6,18 @@ Website Bản đồ du lịch.
 
 - Client: React Js, SCSS, React-leaflet (OSM : Open street map)
 - Server: Node JS
+
+# Configuration
+
+1. Install `Node Js` `npm`
+2. Install packages:
+
+- At client dir: `npm install`
+- At server dir: `npm install`
+
+3. Run: at root dir:
+
+- Run server: `cd ./server; npm start`
+- Run client: `cd ./client; npm start`
+
+4. Access: `http://localhost:3000`
--- server/src/index.js
+++ server/src/index.js
@@ -6,6 +6,26 @@ const path = require("path");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const convertVNtoEng = require("./helpers/convertVNtoEng");
+const multer = require("multer");
+const { escape } = require("querystring");
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    var Path = path.join(__dirname, "..", "public", "uploads");
+    callback(null, Path);
+  },
+  filename: (req, file, callback) => {
+    var fileID = path.extname(file.originalname);
+    var userId = req.userId;
+
+    callback(
+      null,
+      file.originalname.substring(0, file.originalname.lastIndexOf(".")) +
+      fileID
+    );
+  },
+});
+
+const upload = multer({ storage });
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -32,8 +52,6 @@ app.post("/points/data", async function (req, res, next) {
     points: data.points,
   });
 
-  // console.log("[data]", points);
-
   await fs.writeFileSync(
     path.join(__dirname, "..", "public", "uploads", "points.json"),
     JSON.stringify(points)
@@ -49,6 +67,55 @@ app.get("/points/data", async function (req, res, next) {
   );
 
   res.send(data);
+});
+
+app.get("/points/data", async function (req, res, next) {
+  const data = await fs.readFileSync(
+    path.join(__dirname, "..", "public", "uploads", "points.json"),
+    "utf-8"
+  );
+
+  res.send(data);
+});
+
+app.post("/send-image", upload.single('image'), async function (req, res, next) {
+  const data = req.body;
+
+  if (req.file.filename) req.file.url = `/uploads/${req.file.filename}`;
+  let currentData = await fs.readFileSync(
+    path.join(__dirname, "..", "public", "uploads", "reviews.json"),
+    "utf-8"
+  );
+
+  // convert to Array
+  currentData = JSON.parse(currentData);
+  currentData = Array.from(currentData);
+
+  const item = {
+    id: currentData.length,
+    user: data.user,
+    description: data.description,
+    location: { lat: data.location_lat, lng: data.location_lng },
+    image: req.file,
+  }
+
+  currentData.push(item);
+
+  await fs.writeFileSync(
+    path.join(__dirname, "..", "public", "uploads", "reviews.json"),
+    JSON.stringify(currentData)
+  );
+
+  res.send(item);
+});
+
+app.get('/get-image', async (req, res) => {
+  let data = await fs.readFileSync(path.join(__dirname, "..", "public", "uploads", "reviews.json"), 'utf8');
+  res.send(data);
+});
+
+app.get("/", (req, res) => {
+  res.send({ 'title': 'Harryguci' })
 });
 
 app.listen(port, () => console.log(`listen on http://localhost:${port}`));
